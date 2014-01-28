e7a7440c5157ef7843aaf4f76c5ea0f8d8f187c3
Joe Green <joe.green0991@gmail.com>
2014-01-28 09:06:54 +0000

    -

---
+++ resources/capabilities.php
@@ -0,0 +1,258 @@
+<?php return array (
+  'HTC' => 
+  array (
+    0 => 
+    array (
+      'regex' => 'HTC',
+    ),
+  ),
+  'Tesla' => 
+  array (
+    0 => 
+    array (
+      'regex' => 'Tesla',
+    ),
+  ),
+  'SonyEricsson' => 
+  array (
+    0 => 
+    array (
+      'regex' => 'Ericsson',
+    ),
+  ),
+  'Sony' => 
+  array (
+    0 => 
+    array (
+      'regex' => 'PlayStation',
+    ),
+  ),
+  'Amazon' => 
+  array (
+    0 => 
+    array (
+      'regex' => 'Kindle',
+    ),
+  ),
+  'Nokia' => 
+  array (
+    0 => 
+    array (
+      'regex' => 'Nokia',
+    ),
+    1 => 
+    array (
+      'regex' => 'Lumia',
+    ),
+  ),
+  'RIM' => 
+  array (
+    0 => 
+    array (
+      'regex' => 'BlackBerry',
+    ),
+  ),
+  'Palm' => 
+  array (
+    0 => 
+    array (
+      'regex' => 'Palm',
+    ),
+  ),
+  'HP' => 
+  array (
+    0 => 
+    array (
+      'regex' => 'HP',
+    ),
+  ),
+  'Apple' => 
+  array (
+    0 => 
+    array (
+      'regex' => 'AppleTV',
+    ),
+    1 => 
+    array (
+      'regex' => '^iPod',
+    ),
+    2 => 
+    array (
+      'regex' => '^iPad',
+    ),
+    3 => 
+    array (
+      'regex' => '^iPhone',
+    ),
+  ),
+  'Acer' => 
+  array (
+    0 => 
+    array (
+      'regex' => 'Acer',
+    ),
+  ),
+  'Alcatel' => 
+  array (
+    0 => 
+    array (
+      'regex' => 'Alcatel',
+    ),
+  ),
+  'Amoi' => 
+  array (
+    0 => 
+    array (
+      'regex' => 'Amoi',
+    ),
+  ),
+  'Asus' => 
+  array (
+    0 => 
+    array (
+      'regex' => 'Asus',
+    ),
+  ),
+  'Bird' => 
+  array (
+    0 => 
+    array (
+      'regex' => 'Bird',
+    ),
+  ),
+  'Dell' => 
+  array (
+    0 => 
+    array (
+      'regex' => 'Dell',
+    ),
+  ),
+  'DoCoMo' => 
+  array (
+    0 => 
+    array (
+      'regex' => 'DoCoMo',
+    ),
+  ),
+  'Huawei' => 
+  array (
+    0 => 
+    array (
+      'regex' => 'Huawei',
+    ),
+  ),
+  'i-mate' => 
+  array (
+    0 => 
+    array (
+      'regex' => 'i\\-mate',
+    ),
+  ),
+  'Kyocera' => 
+  array (
+    0 => 
+    array (
+      'regex' => 'Kyocera',
+    ),
+  ),
+  'Lenovo' => 
+  array (
+    0 => 
+    array (
+      'regex' => 'Lenovo',
+    ),
+  ),
+  'HbbTV' => 
+  array (
+    0 => 
+    array (
+      'regex' => 'HbbTV',
+    ),
+  ),
+  'LG' => 
+  array (
+    0 => 
+    array (
+      'regex' => 'LG',
+    ),
+  ),
+  'Microsoft' => 
+  array (
+    0 => 
+    array (
+      'regex' => 'KIN',
+    ),
+  ),
+  'Motorola' => 
+  array (
+    0 => 
+    array (
+      'regex' => 'Motorola',
+    ),
+    1 => 
+    array (
+      'regex' => 'Razr',
+    ),
+  ),
+  'Nintendo' => 
+  array (
+    0 => 
+    array (
+      'regex' => 'Nintendo',
+    ),
+  ),
+  'Pantech' => 
+  array (
+    0 => 
+    array (
+      'regex' => 'Pantech',
+    ),
+  ),
+  'Philips' => 
+  array (
+    0 => 
+    array (
+      'regex' => 'Philips',
+    ),
+  ),
+  'Samsung' => 
+  array (
+    0 => 
+    array (
+      'regex' => 'Samsung',
+    ),
+    1 => 
+    array (
+      'regex' => '^S.H-',
+    ),
+  ),
+  'Sega' => 
+  array (
+    0 => 
+    array (
+      'regex' => 'Dreamcast',
+    ),
+  ),
+  'Softbank' => 
+  array (
+    0 => 
+    array (
+      'regex' => 'Softbank',
+    ),
+  ),
+  'WebTV' => 
+  array (
+    0 => 
+    array (
+      'regex' => 'WebTV',
+    ),
+  ),
+  'Generic' => 
+  array (
+    0 => 
+    array (
+      'regex' => '(hiptop|avantgo|plucker|xiino|blazer|elaine|up.browser|up.link|mmp|smartphone|midp|wap|vodafone|orange|o2|pocket|mobile|pda)',
+      'manufacturer' => '$1',
+    ),
+  ),
+);
