f07a787dad1a064c2f9d5b60787774eada567e6e
Joe Green <joe.green0991@gmail.com>
2015-03-27 10:23:40 +0000

    Update TestCommand.php

--- src/UAParser/Command/TestCommand.php
+++ src/UAParser/Command/TestCommand.php
@@ -5,30 +5,110 @@ use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Yaml\Yaml;
 use UAParser\Parser;
 
 class TestCommand extends Command
 {
+    private $yamlParser;
+    private $parser;
+
     protected function configure()
     {
         $this
             ->setName('test')
             ->setDescription('Runs the tests against the parser.')
             ->addArgument('uastring', InputArgument::OPTIONAL, 'A user agent string to test')
+            ->addOption('tests', 't', InputOption::VALUE_IS_ARRAY | InputOption::VALUE_REQUIRED, 'The test files, or a folder containing the test data files in yaml format')
         ;
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $parser = new Parser();
+        $this->yamlParser = new Yaml();
+
+        $this->parser = new Parser();
 
         if($ua = $input->getArgument('uastring'))
         {
-            $string = $parser->parse($ua, true);
+            $string = $this->parser->parse($ua, true);
 
             $output->writeln($ua);
 
             $output->writeln(json_encode($string, JSON_PRETTY_PRINT));
+
+            return;
         }
+
+        $testFiles = $input->getOption('tests');
+
+        $testTypes = array('os', 'device', 'user_agent');
+
+        foreach($testFiles as $testFile)
+        {
+            $tests = $this->loadTestFile($testFile, $output);
+
+            foreach($testTypes as $type)
+            {
+                if(!isset($tests[$type])) continue;
+
+                foreach($tests[$type] as $test)
+                {
+                    $this->runTest($test, $type);
+                }
+            }
+        }
+    }
+
+    private function runTest($test, $type)
+    {
+        if(isset($test['js_ua'])) unset($test['js_ua']);
+        if(isset($test['js_user_agent_v1'])) unset($test['js_user_agent_v1']);
+
+        $ua = $test['user_agent_string'];
+        unset($test['user_agent_string']);
+
+        $parsed = $this->parser->parse($ua);
+
+        $diff = array_diff((array)$parsed[$type], $test);
+
+        if($diff)
+        {
+            echo "Test failed: $ua\n";
+            echo "Expected:\n";
+            echo json_encode($test, JSON_PRETTY_PRINT);
+            echo "\nGot:\n";
+            echo json_encode($parsed[$type], JSON_PRETTY_PRINT);
+            echo "\n\n";
+        }
+    }
+
+    private function loadTestFile($testFile, OutputInterface $output)
+    {
+        if(is_dir($testFile))
+        {
+            $arr = array();
+
+            foreach(new \RecursiveIteratorIterator(new \RecursiveDirectoryIterator($testFile, \RecursiveDirectoryIterator::SKIP_DOTS)) as $file)
+            {
+                if(preg_match('/\.(yml|yaml)/', $file))
+                {
+                    $arr = array_merge_recursive($this->loadTestFile($file, $output), $arr);
+                }
+            }
+
+            return $arr;
+        }
+
+        if(!preg_match('/^http(s)?:\/\//', $testFile) && !is_file($testFile))
+        {
+            throw new \Exception('Path ' . $testFile . ' is not a file or directory');
+        }
+
+        $tests = file_get_contents($testFile);
+
+        $output->writeln("Loaded file: $testFile");
+
+        return $this->yamlParser->parse($tests);
     }
 }
